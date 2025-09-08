8cb224eaccb9abc9a409a9c30b214750555691c8
Yi888a <Ziyil8@nycstudents.net>
2025-09-08 09:24:52 -0400

    added turtle stuff

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("hello World")
+import turtle
+from turtle import *
+t = Turtle()
+def equal(x):
+    t.forward(x)
+    t.left(21)
+    t.forward(x)
+    t.right(99)
+    t.forward(x) 
+    t.right(67)
+    t.left(41)
+equal(222)
